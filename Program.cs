839208b2de427f2d609e0721449c6a935aecbe69
SofiaFriberg <77665339+SofiaFriberg@users.noreply.github.com>
2021-01-25 09:30:24 +0100

    introducing bool playAgain (for real)
    First one was a fail..

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace Uppgift_9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            Console.WriteLine("First one to 3p wins, Let's GO!");
+
+            string inputPlayer = null;
+    
+
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                int pointsPlayer = 0;
+                int pointsComputer = 0;
+
+                while (pointsPlayer < 3 && pointsComputer < 3)
+                {
+                    Console.WriteLine("Choose between ROCK, PAPER and SCISSORS");
+                    inputPlayer = Console.ReadLine();
+                    inputPlayer = inputPlayer.ToUpper();
+
+                    
+
+
+
+
+                }
+
+
+
+            }
+
+
+
             Console.ReadKey();
         }
     }
